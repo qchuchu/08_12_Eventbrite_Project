66a081c062e1e1c1071b2bfa353af83ceec309de
qchuchu <quentin.churet@outlook.fr>
2019-08-12 17:01:35 +0200

    Final Commit

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,8 +6,8 @@ class Event < ApplicationRecord
 
   validates :start_date, presence: true, start_date: true
   validates :duration, presence: true, duration: true
-  validates :title, presence: true, length { in: 5..140 }
-  validates :description, presence: true, length { in: 20..1000 }
+  validates :title, presence: true, length: { in: 5..140 }
+  validates :description, presence: true, length: { in: 20..1000 }
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 1000 }
   validates :location, presence: true
 end
